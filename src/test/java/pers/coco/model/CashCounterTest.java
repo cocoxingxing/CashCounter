af26af8087000a907262335ba9be618c31319d60
kehanliu <kehanliu@tju.edu.cn>
2016-07-19 19:38:17 +0800

    1.add function printShoppingList to CashCounter for printing the shopping list

--- src/test/java/pers/coco/model/CashCounterTest.java
+++ src/test/java/pers/coco/model/CashCounterTest.java
@@ -14,9 +14,7 @@ public class CashCounterTest {
     public void should_use_3for2_discount_when_discount_item_ITEM000005() {
         CashCounter cashCounter = new CashCounter();
         ShoppingItem item  = cashCounter.discountItem("ITEM000005", 3);
-        System.out.println("discount="+item.getDiscountPrice());
         assertEquals(Consts.DISCOUNT_3_FOR_2_TAG, item.getDiscountTag());
         assertEquals(3, (int)item.getDiscountPrice());
     }
-
 }
