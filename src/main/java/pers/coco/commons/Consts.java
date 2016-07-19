f66c4ae75e70170f30db41cc8b04e2c14c32386e
kehanliu <kehanliu@tju.edu.cn>
2016-07-19 18:05:46 +0800

    1.add package commons for holding the commonly used classes 2.add class Consts for storing some common variables

---
+++ src/main/java/pers/coco/commons/Consts.java
@@ -0,0 +1,14 @@
+package pers.coco.commons;
+
+/**
+ * Created by Administrator on 2016/7/17.
+ */
+public class Consts {
+    public static String ITEM_FILE_PATH = "././data/shoppingItem.txt";
+    public static String DISCOUNT_FILE_PATH = "././data/discountSetting.json";
+    public static String DB_FILE_PATH = "././data/commodityDB.json";
+
+    public static String DISCOUNT_3_FOR_2_TAG = "3for2";
+    public static String DISCOUNT_95_TAG = "95%";
+    public static String DISCOUNT_NONE_TAG = "";
+}
