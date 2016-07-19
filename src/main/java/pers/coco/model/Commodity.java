78df07810e5af1f4181cac80ad8d0fb4d54c3b1f
kehanliu <kehanliu@tju.edu.cn>
2016-07-19 17:57:21 +0800

    1.add package pers.coco.model 2.add class Commodity for holding the commodity in the file commodityDB.json 3.add class ShoppingItem for holding the item in the shopping list

---
+++ src/main/java/pers/coco/model/Commodity.java
@@ -0,0 +1,39 @@
+package pers.coco.model;
+
+/**
+ * Created by Administrator on 2016/7/19.
+ */
+public class Commodity {
+    private String name = "";
+    private String unit = "";
+    private double price = 0.0d;
+    private String code = "";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+    public String getUnit() {
+        return this.unit;
+    }
+}
---
+++ src/main/java/pers/coco/model/ShoppingItem.java
@@ -0,0 +1,63 @@
+package pers.coco.model;
+
+/**
+ * Created by Administrator on 2016/7/19.
+ */
+public class ShoppingItem {
+    private String name = "";
+    private int num = 0;
+    private double price = 0.0d;
+    private String unit = "";
+    private double discountPrice = 0.0d;
+    private String discountTag = "";
+    private double totalPrice = 0.0d;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    public int getNum() {
+        return this.num;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+    public String getUnit() {
+        return this.unit;
+    }
+
+    public void setDiscountPrice(double discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+    public double getDiscountPrice() {
+        return this.discountPrice;
+    }
+
+    public void setDiscountTag(String discountTag) {
+        this.discountTag = discountTag;
+    }
+    public String getDiscountTag() {
+        return this.discountTag;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    public double getTotalPrice() {
+        return this.totalPrice;
+    }
+}
