8c3f900094446a24a86acead24e5b823728ee4b1
kehanliu <kehanliu@tju.edu.cn>
2016-07-19 19:41:13 +0800

    1.add package pers.coco.main 2.add class Main as the project entry

---
+++ src/main/java/pers/coco/main/Main.java
@@ -0,0 +1,14 @@
+package pers.coco.main;
+
+import pers.coco.model.CashCounter;
+
+/**
+ * Created by Administrator on 2016/7/19.
+ */
+public class Main {
+    public static CashCounter cashCounter = new CashCounter();
+
+    public static void main(String[] args) {
+        
+    }
+}
